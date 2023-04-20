5a38c775dda8f02763184a3ad2600a2730310ebd
Aleksey Kachalov <a_kachalov@mail.ru>
2023-04-20 19:58:14 +0300

    add seminar 4

--- GettingToKnowPython/SeminarProduct/Seminar4/Task2_27.py
+++ GettingToKnowPython/SeminarProduct/Seminar4/Task2_27.py
@@ -2,11 +2,8 @@
 последовательность непробельных символов идущих подряд, 
 слова разделены одним или большим числом пробелов. Определите, сколько различных слов
 содержится в этом тексте.
-Input: She sells sea shells on the sea shore The shells
-that she sells are sea shells I'm sure.So if she sells sea
-shells on the sea
+Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea
 """
 
-w = input().split()
-
-print((set(w)))
+w = input().upper().split()
+print((set(len(w))))
